e07247c73a2a16b7c8c7c0ea22ea110da006dcf3
Hans <r.hans4486@gmail.com>
2020-06-01 14:45:19 -0400

    Adding comment

--- app.js
+++ app.js
@@ -11,57 +11,59 @@ app.use(express.static('public'));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// When someone go to the homepage
 app.get('/', (req, res) => {
 	res.render('home');
 });
 
+// When someone go to the login page
 app.get('/login', (req, res) => {
 	res.render('login');
 });
 
+// When someone try to connect
 app.post('/login', (req, res) => {
+	// Infos provid by the user
 	const email = req.body.username;
 	const password = req.body.password;
 
+	// Find if the user exist
 	let sql = "SELECT * FROM user  WHERE email = '" + email + "'";
-
 	connexion.query(sql, function (err, result) {
 		if (err) throw err;
 		console.log(result);
 		
 		if (result[0]) {
 			console.log(result[0].password);
+			// Compare the password that user provide with password in the database
 			bycrypt.compare(password, result[0].password, (err, result) => {
 				result ? res.render('secrets') : console.log('WRONG PASSWORD');
 			});
 		} else {
-
 			console.log('NO USER FOUND');
 			res.redirect('/login');
 		}
-		// result ? res.render('secrets') : res.redirect('/login');
 	});
 });
 
+// When someone go to the register page
 app.get('/register', (req, res) => {
 	res.render('register');
 });
 
+// When someone try to register
 app.post('/register', (req, res) => {
+	// Information retrive to the register form
 	const email = req.body.username;
 	const password = req.body.password;
 	const saltRounds = 10;
 
+	// hash the password
 	bycrypt.hash(password, saltRounds, (err, hash) => {
 		if(err) throw err;
 		
 		let sql =
-			"INSERT INTO user (email, password) VALUES ('" +
-			email +
-			"','" +
-			hash +
-			"')";
-
+			"INSERT INTO user (email, password) VALUES ('" + email +"','" +hash +"')";
 		connexion.query(sql, function (err, result) {
 			if (err) throw err;
 			console.log(result);
